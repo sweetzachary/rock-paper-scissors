a2ea624ab369f5dd5bfb7c644eb97174bb371733
sweetzachary <a.ntigr16@gmail.com>
2021-11-20 19:15:48 +0200

    Fix floor function reference in script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 let options = ['Rock', 'Paper', 'Scissors']
 
 function computerPlay() {
-    let choice = floor(Math.random()*3);
+    let choice = Math.floor(Math.random()*3);
     return options[choice];
 }
