5dbedb31625420795a6825f373ce854c8fd488c0
sweetzachary <a.ntigr16@gmail.com>
2021-11-20 20:36:10 +0200

    Add game() function containing the main game loop

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const win_table = [['tie', 'lose', 'win'],
                 ['lose', 'win','tie']]
 
 function computerPlay() {
-    let choice = Math.floor(Math.random()*3);
-    return choice;
+    let option = Math.floor(Math.random()*3);
+    return choice[option];
 }
 
 function gameRound(playerChoice, computerChoice) {
@@ -16,9 +16,34 @@ function gameRound(playerChoice, computerChoice) {
     const computerChoiceNum = choice.indexOf(computerChoice);
     return win_table[playerChoiceNum][computerChoiceNum];
 }
-/*
-function game() {
-    for(let i = 1; i <= 5; i++) {
 
+function game() {
+    let wins = 0;
+    let playerChoice;
+    let computerChoice;
+    let result;
+    for(let i = 1; i <= 5; i++) {
+        playerChoice = prompt("choose your weapon!");
+        computerChoice = computerPlay();
+        result = gameRound(playerChoice, computerChoice);
+        if(result == 'win') {
+            wins++;
+        }
+        switch(result){
+            case 'win':
+                console.log(`Your ${playerChoice} beats ${computerChoice}!`)
+                break;
+            case 'tie':
+                console.log(`A tie!`);
+                break;
+            case 'lose':
+                console.log(`Your ${playerChoice} is severely beaten by ${computerChoice}!`)
+
+        }
     }
-}*/
+    if(wins >= 3) {
+        console.log(`You win!`)
+    } else {
+        console.log(`You lost!!!`)
+    }
+}
