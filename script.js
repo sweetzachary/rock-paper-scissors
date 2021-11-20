c158bd08417a43caadceee0f03ebd8e430bc6289
sweetzachary <a.ntigr16@gmail.com>
2021-11-20 19:57:47 +0200

    Add gameRound(playerChoice, computerChoice) function

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 /*playing options*/
-let options = ['Rock', 'Paper', 'Scissors']
+const choice =['rock', 'paper', 'scissors']
+
+let win_table = [['tie', 'lose', 'win'],
+                ['win', 'tie', 'lose'],
+                ['lose', 'win','tie']]
 
 function computerPlay() {
     let choice = Math.floor(Math.random()*3);
-    return options[choice];
+    return choice;
 }
+
+function gameRound(playerChoice, computerChoice) {
+    return win_table[playerChoice][computerChoice];
+}
+
